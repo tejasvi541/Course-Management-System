ff0ab4c146ee55f5285363eb84c8da7b4609debf
Tejasvi <tejasvi.tanwar541@gmail.com>
2023-10-04 13:55:04 +0530

    Course edit form and PATCH api for courses

---
+++ app/api/courses/[courseId]/route.ts
@@ -0,0 +1,34 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { courseId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const { courseId } = params;
+    const values = await req.json();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const course = await db.course.update({
+      where: {
+        id: courseId,
+        userId,
+      },
+      data: {
+        ...values,
+      },
+    });
+
+    return NextResponse.json(course);
+  } catch (error) {
+    console.log("[COURSE_ID]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
