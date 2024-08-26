5f0108e1c3ba599e04cb260aebe8e75fba8803e4
Tejasvi <tejasvi.tanwar541@gmail.com>
2024-08-26 16:47:43 -0400

    Migration changes and Dependency issues resolved

---
+++ prisma/migrations/20240826195419_init/migration.sql
@@ -0,0 +1,70 @@
+-- CreateTable
+CREATE TABLE `Chapter` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `description` TEXT NULL,
+    `videoUrl` TEXT NULL,
+    `position` INTEGER NOT NULL,
+    `isPublished` BOOLEAN NOT NULL DEFAULT false,
+    `isFree` BOOLEAN NOT NULL DEFAULT false,
+    `courseId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `Chapter_courseId_idx`(`courseId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `MuxData` (
+    `id` VARCHAR(191) NOT NULL,
+    `assetId` VARCHAR(191) NOT NULL,
+    `playbackId` VARCHAR(191) NULL,
+    `chapterId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `MuxData_chapterId_key`(`chapterId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `UserProgress` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `chapterId` VARCHAR(191) NOT NULL,
+    `isCompleted` BOOLEAN NOT NULL DEFAULT false,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `UserProgress_chapterId_idx`(`chapterId`),
+    UNIQUE INDEX `UserProgress_userId_chapterId_key`(`userId`, `chapterId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Purchase` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `courseId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `Purchase_courseId_idx`(`courseId`),
+    UNIQUE INDEX `Purchase_userId_courseId_key`(`userId`, `courseId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `StripeCustomer` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `stripeCustomerId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `StripeCustomer_userId_key`(`userId`),
+    UNIQUE INDEX `StripeCustomer_stripeCustomerId_key`(`stripeCustomerId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateIndex
+CREATE FULLTEXT INDEX `Course_title_idx` ON `Course`(`title`);
