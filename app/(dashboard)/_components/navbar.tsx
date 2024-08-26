db300c6f8310d67d2c229b819a83b4d2cd99ccc3
Tejasvi <tejasvi.tanwar541@gmail.com>
2024-08-25 22:32:29 -0400

    Export issues solved, invarint also

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import MobileSidebar from "./mobile-sidebar";
-import NavbarRoutes from "@/components/navbar-routes";
+import { NavbarRoutes } from "@/components/navbar-routes";
 const Navbar = () => {
   return (
     <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
